6a6cb8480c4c4850521e2ae9398ce87cb1e0ab92
korabelnikov <korabelnikov@rza.ru>
2025-04-07 19:41:35 +0300

    stage

--- Tests.cs
+++ Tests.cs
@@ -6,25 +6,89 @@ namespace register_packager;
 public class Tests(ITestOutputHelper testOutputHelper)
 {
     [Theory]
-    [InlineData(8, new int[] { }, 8, 9, 12)]
-    [InlineData(5, new int[] { }, 2, 3, 4, 5, 6)]
-    [InlineData(17, new int[] { }, 1, 10, 25)]
-    [InlineData(12, new int[] { }, 1, 10, 25)]
-    [InlineData(26, new int[] { }, 1, 25, 45)]
-    [InlineData(5,  new [] { 3 }, 1, 4, 5, 8, 9, 44)]
-    [InlineData(4,  new [] { 3 }, 1, 4, 5, 8, 9, 44)]
-    public void Sample1(int limit, int[] holes, params int[] addresses)
+    [InlineData(8,  8, 9, 12)]
+    [InlineData(5,  2, 3, 4, 5, 6)]
+    [InlineData(17,  1, 10, 25)]
+    [InlineData(12,  1, 10, 25)]
+    [InlineData(26,  1, 25, 45)]
+    [InlineData(5,  1, 4, 5, 8, 9, 44)]
+    [InlineData(4,  1, 4, 5, 8, 9, 44)]
+    [InlineData(10,  1, 10)]
+    public void Sample1(int maxLimit, params int[] registers)
     {
-        var chunks = Algorithm.Solve(addresses, holes.Select(x => (x, x)).ToArray(), limit);
-        testOutputHelper.WriteLine($"{string.Join(", ", chunks.Select(x => $"[{string.Join(", ", x)}]"))}");
+        var o = Algorithm.Solve(maxLimit, registers);
+        testOutputHelper.WriteLine($"[{string.Join(", ", o.Select(x => $"[{string.Join(", ", x)}]"))}]");
     }
     [Fact]
     public void Sample2()
     {
-        var limit = 16;
-        var addresses = Enumerable.Range(0, 1000).Select(x => Random.Shared.Next(0, 5000)).OrderBy(x => x).ToArray();
-        var holes = Enumerable.Range(0, 30).Select(x => (Random.Shared.Next(0, 1000), Random.Shared.Next(1000, 5000))).OrderBy(x => x).ToArray();
-        var chunks = Algorithm.Solve(addresses, holes, limit);
-        testOutputHelper.WriteLine($"{string.Join(", ", chunks.Select(x => $"[{string.Join(", ", x)}]"))}");
+        int[][] reg = [[2, 3, 6, 8, 15], [19, 20, 26, 27, 30], [41, 52, 53], [59, 60, 69, 75], [76, 77, 85, 90], [97, 99]];
+        int[][] expected = [[2, 3, 6, 8], [15, 19, 20, 26, 27, 30], [41], [52, 53, 59, 60], [69, 75, 76, 77], [85, 90, 97, 99]];
+        var chunks = Algorithm.Chunk(16, reg.SelectMany(x => x).ToArray()).ToArray();
+        testOutputHelper.WriteLine($"{string.Join(", ", chunks.Select(x => $"[{string.Join(", ", x)}]"))} -> [Chunks = {chunks.Length} Garbage = {chunks.Sum(Algorithm.CalculateGarbage)}]");
+        var o = Algorithm.Solve(16, reg.SelectMany(x => x).ToArray()).ToArray();
+        testOutputHelper.WriteLine($"{string.Join(", ", expected.Select(x => $"[{string.Join(", ", x)}]"))} -> [Chunks = {expected.Length} Garbage = {expected.Sum(Algorithm.CalculateGarbage)}]");
+        testOutputHelper.WriteLine($"{string.Join(", ", o.Select(x => $"[{string.Join(", ", x)}]"))} -> [Chunks = {o.Length} Garbage = {o.Sum(Algorithm.CalculateGarbage)}]");
+        Assert.Equal(reg.SelectMany(x => x), o.SelectMany(x => x));
+        Assert.True(o.Length <= expected.Length);
+        Assert.All(o, x => Assert.False(Algorithm.ExcessLimit(16, x, out _, out _)));
+        Assert.True(o.Sum(Algorithm.CalculateGarbage) <= expected.Sum(Algorithm.CalculateGarbage));
+    }
+    [Theory]
+    [InlineData(4, 100_00)]
+    [InlineData(256, 10_000)]
+    [InlineData(256, 100_000)]
+    public void SampleMax(int max, int count)
+    {
+        var reg = Enumerable.Range(0, count)
+            .Select(x => Random.Shared.Next(0, count))
+            .Distinct()
+            .OrderBy(x => x)
+            .ToArray();
+        var chunks = Algorithm.Chunk(max, reg).ToArray();
+        testOutputHelper.WriteLine($"{string.Join(", ", chunks.Select(x => $"[{string.Join(", ", x)}]"))} -> [Chunks = {chunks.Length} Garbage = {chunks.Sum(Algorithm.CalculateGarbage)}]");
+        var o = Algorithm.Solve(max, reg).ToArray();
+        testOutputHelper.WriteLine($"{string.Join(", ", o.Select(x => $"[{string.Join(", ", x)}]"))} -> [Chunks = {o.Length} Garbage = {o.Sum(Algorithm.CalculateGarbage)}]");
+        Assert.Equal(reg, o.SelectMany(x => x));
+        Assert.All(o, x => Assert.False(Algorithm.ExcessLimit(max, x, out _, out _)));
+        Assert.True(o.Length <= chunks.Length);
+        Assert.True(o.Sum(Algorithm.CalculateGarbage) < chunks.Sum(Algorithm.CalculateGarbage));
+    }
+    [Theory]
+    [InlineData(13, 1, 3, 5, 9, 10, 11, 13)]
+    [InlineData(4, 1, 3, 5, 9, 10, 11, 13)]
+    [InlineData(1, 1, 3, 5, 9, 10, 11, 13)]
+    [InlineData(8,  8, 9, 12)]
+    [InlineData(5,  2, 3, 4, 5, 6)]
+    [InlineData(17,  1, 10, 25)]
+    [InlineData(12,  1, 10, 25)]
+    [InlineData(26,  1, 25, 45)]
+    [InlineData(5,  1, 4, 5, 8, 9, 44)]
+    [InlineData(4,  1, 4, 5, 8, 9, 44)]
+    [InlineData(10,  1, 10)]
+    public void Chunk(int maxLimit, params int[] registers)
+    {
+        var variation = Algorithm.Chunk(maxLimit, registers).ToArray();
+        testOutputHelper.WriteLine(string.Join(", ", variation.Select(x => $"[{string.Join(", ", x)}]")));
+    }
+    [Fact]
+    public void Combine()
+    {
+        int[] ch1 = [1, 2, 3, 4, 5];
+        int[] ch2 = [6, 7, 8, 9, 10];
+        foreach (var (trimLeft, joinRight) in Algorithm.Combine(ch1, ch2))
+        {
+            testOutputHelper.WriteLine($"[{string.Join(", ", trimLeft)}], [{string.Join(", ", joinRight)}]");
+        }
+    }
+    [Fact]
+    public void Combine2()
+    {
+        int[] ch1 = [8];
+        int[] ch2 = [9];
+        foreach (var (trimLeft, joinRight) in Algorithm.Combine(ch1, ch2))
+        {
+            testOutputHelper.WriteLine($"[{string.Join(", ", trimLeft)}], [{string.Join(", ", joinRight)}]");
+        }
     }
 }
